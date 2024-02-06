d61ce5b51596273bfa4dc33ffecbc10172945478
Joshua Biong <joshuabiong7@gmail.com>
2024-02-07 00:29:38 +0800

    done with UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
     
-      <div className="card">
-        <h1>{count}</h1>
+      <div className="container">
+        {/* <h1>{count}</h1>
         <button onClick={() => setCount((count) => count + 1)}>
         +  </button>
         <button onClick={() => setCount((count) => count - 1)}>
           -
-        </button>
+        </button> */}
+
+        <div className="cards">
+          <div className="value">0</div>
+          <div className="label">CHILD</div>
+          <div className="buttons">
+            <button>-</button>
+            <button>+</button>
+          </div>
+        </div>
+        <div className="cards">
+          <div className="value">0</div>
+          <div className="label">ADULT</div>
+          <div className="buttons">
+            <button>-</button>
+            <button>+</button>
+          </div>
+        </div>
       </div>
      
     </>
